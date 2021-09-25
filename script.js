298306b149e36b8e794957d00f8c30cdadbf21d3
amir <aaa@gmail.com>
2021-09-25 03:57:22 +0330

    updated

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let txt = document.querySelector("#text")
 let easy = document.querySelector("#easy")
 let butt = document.querySelector("#butt")
-txt.addEventListener("input",function(){easy.innerText=txt.value})
+
+txt.addEventListener("input", function(){
+  easy.innerText = txt.value
+})
+
 butt.addEventListener("click",function(){
-navigator.clipboard.readText().then(text => easy.innerText=text)
+  navigator.clipboard.readText().then(function(text) {
+    text = text.replace(/-$\n/gm, "")
+    easy.innerText = text
+  })
 })
 
