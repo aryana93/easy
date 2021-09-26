6c74f9001854256ca0b1a87c2e1f6813527de71f
amir <aaa@gmail.com>
2021-09-26 05:00:17 +0330

    update

--- script.js
+++ script.js
@@ -3,11 +3,29 @@ let easy = document.querySelector("#easy")
 let paste = document.querySelector("#paste")
 let inc = document.querySelector("#inc")
 let dec = document.querySelector("#dec")
-let bg=document.querySelector('#bg')
-let fg =document.querySelector('#fg')
-let wrapper=document.querySelector("#wrapper")
+let incwidth = document.querySelector("#incwidth")
+let decwidth = document.querySelector("#decwidth")
+let bg = document.querySelector('#bg')
+let fg = document.querySelector('#fg')
+let wrapper = document.querySelector("#wrapper")
 let arabiccheck = document.querySelector("#arabic")
 let arabicfont = "Noto Sans Arabic"
+
+if(localStorage.getItem('maxwidth')){
+  wrapper.style.maxWidth=localStorage.getItem('maxwidth')
+}else{
+  wrapper.style.maxWidth='50rem'
+}
+
+incwidth.addEventListener('click',function(e){
+  wrapper.style.maxWidth = String(Number(wrapper.style.maxWidth.replace('rem', '')) + 0.25) + 'rem'
+  localStorage.setItem('maxwidth',wrapper.style.maxWidth)
+})
+
+decwidth.addEventListener('click',function(e){
+  wrapper.style.maxWidth = String(Number(wrapper.style.maxWidth.replace('rem', '')) - 0.25) + 'rem'
+  localStorage.setItem('maxwidth',wrapper.style.maxWidth)
+})
 
 arabiccheck.addEventListener('input', function(e) {
   if (e.target.checked) {
@@ -18,7 +36,7 @@ arabiccheck.addEventListener('input', function(e) {
     easy.style.fontFamily = "easy"
     easy.style.direction = "ltr"
     localStorage.setItem('arabic', 'unckecked')
-    }
+  }
 })
 
 if (localStorage.getItem('arabic')) {
@@ -33,92 +51,91 @@ if (localStorage.getItem('arabic')) {
   }
 }
 
-if (localStorage.getItem('maxwidth')){
-  wrapper.style.maxWidth=localStorage.getItem('maxwidth')
+if (localStorage.getItem('maxwidth')) {
+  wrapper.style.maxWidth = localStorage.getItem('maxwidth')
 }
 
-if (localStorage.getItem('easy')){
+if (localStorage.getItem('easy')) {
   easy.innerText = localStorage.getItem('easy')
 }
 
-if(localStorage.getItem('color')){
-  easy.style.color=localStorage.getItem('color')
+if (localStorage.getItem('color')) {
+  easy.style.color = localStorage.getItem('color')
   fg.value = localStorage.getItem('color')
-}else{
-  easy.style.color="#000000"
+} else {
+  easy.style.color = "#000000"
 }
 
-if(localStorage.getItem('background')){
-  easy.style.backgroundColor=localStorage.getItem('background')
-    bg.value = localStorage.getItem('background')
-}else{
-  easy.style.backgroundColor="#ffffff"
+if (localStorage.getItem('background')) {
+  easy.style.backgroundColor = localStorage.getItem('background')
+  bg.value = localStorage.getItem('background')
+} else {
+  easy.style.backgroundColor = "#ffffff"
 }
 
-fg.addEventListener('input',function(e){
+fg.addEventListener('input', function(e) {
   easy.style.color = (e.target.value)
   localStorage.setItem('color', e.target.value)
 })
 
-bg.addEventListener('input',function(e){
+bg.addEventListener('input', function(e) {
   easy.style.backgroundColor = (e.target.value)
   localStorage.setItem('background', e.target.value)
 })
 
-txt.addEventListener("input", function(){
+txt.addEventListener("input", function() {
   text = txt.value.replace(/-$\n/gm, "")
   text = text.replace(/([^\.])$\n/gm, "$1 ")
   text = text.replace(/\.$/gm, ".\n")
   easy.innerText = text
-  localStorage.setItem('easy',text)
+  localStorage.setItem('easy', text)
 })
 
-paste.addEventListener("click",function(){
+paste.addEventListener("click", function() {
   navigator.clipboard.readText().then(function(text) {
     text = text.replace(/-$\n/gm, "")
     text = text.replace(/([^\.])$\n/gm, "$1 ")
     text = text.replace(/\.$/gm, ".\n")
     easy.innerText = text
-    localStorage.setItem('easy',text)
+    localStorage.setItem('easy', text)
   })
 })
 
 if (localStorage.getItem('fontsize')) {
   easy.style.fontSize = localStorage.getItem('fontsize')
-}else{
+} else {
   easy.style.fontSize = "1rem"
 }
 
-inc.addEventListener("click", function(e){
-  easy.style.fontSize =  String(Number(easy.style.fontSize.replace("rem","")) + 0.25) + "rem"
-  localStorage.setItem('fontsize',easy.style.fontSize)
+inc.addEventListener("click", function(e) {
+  easy.style.fontSize = String(Number(easy.style.fontSize.replace("rem", "")) + 0.25) + "rem"
+  localStorage.setItem('fontsize', easy.style.fontSize)
 })
 
-dec.addEventListener("click", function(e){
-  easy.style.fontSize =  String(Number(easy.style.fontSize.replace("rem","")) - 0.25) + "rem"
-    localStorage.setItem('fontsize',easy.style.fontSize)
+dec.addEventListener("click", function(e) {
+  easy.style.fontSize = String(Number(easy.style.fontSize.replace("rem", "")) - 0.25) + "rem"
+  localStorage.setItem('fontsize', easy.style.fontSize)
 })
 
-document.body.addEventListener("keydown", function (ev) {
+document.body.addEventListener("keydown", function(ev) {
 
-	// function to check the detection
-	ev = ev || window.event; // Event object 'ev'
-	let key = ev.which || ev.keyCode; // Detecting keyCode
-	
-	// Detecting Ctrl
-	let ctrl = ev.ctrlKey ? ev.ctrlKey : ((key === 17) ? true : false);
+  // function to check the detection
+  ev = ev || window.event; // Event object 'ev'
+  let key = ev.which || ev.keyCode; // Detecting keyCode
 
-	// If key pressed is V and if ctrl is true.
-	if (key == 86 && ctrl) {
-		// print in console.
-      navigator.clipboard.readText().then(function(text) {
-        text = text.replace(/-$\n/gm, "")
-        text = text.replace(/([^\.])$\n/gm, "$1 ")
-        text = text.replace(/\.$/gm, ".\n")
-        easy.innerText = text
-        localStorage.setItem('easy',text)
-  })
-	}
-	
+  // Detecting Ctrl
+  let ctrl = ev.ctrlKey ? ev.ctrlKey : ((key === 17) ? true : false);
+
+  // If key pressed is V and if ctrl is true.
+  if (key == 86 && ctrl) {
+    // print in console.
+    navigator.clipboard.readText().then(function(text) {
+      text = text.replace(/-$\n/gm, "")
+      text = text.replace(/([^\.])$\n/gm, "$1 ")
+      text = text.replace(/\.$/gm, ".\n")
+      easy.innerText = text
+      localStorage.setItem('easy', text)
+    })
+  }
+
 }, false);
-
