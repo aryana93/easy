89c449f20d8bf9a92638897130fb0f12733d47a8
javdanmanesh <javdanmanesh@mail.com>
2021-09-29 14:37:46 +0330

    update

--- script.js
+++ script.js
@@ -1,140 +1,180 @@
-let txt = document.querySelector("#text")
-let easy = document.querySelector("#easy")
-let paste = document.querySelector("#paste")
-let inc = document.querySelector("#inc")
-let dec = document.querySelector("#dec")
-let incwidth = document.querySelector("#incwidth")
-let decwidth = document.querySelector("#decwidth")
-let bg = document.querySelector('#bg')
-let fg = document.querySelector('#fg')
-let wrapper = document.querySelector("#wrapper")
-let arabiccheck = document.querySelector("#arabic")
+//get font size button nodes
+let btnincfontsize = document.querySelector('#btnincfontsize')
+let btndecfontsize = document.querySelector("#btndecfontsize")
+
+//get width resize button nodes 
+let btnincwidth = document.querySelector("#btnincwidth")
+let btndecwidth = document.querySelector("#btndecwidth")
+
+//get arabic english checkbox node
+let chkboxaren = document.querySelector("#chkboxaren")
+
+//get color picker nodes
+let clrfg = document.querySelector('#clrfg')
+let clrbg = document.querySelector('#clrbg')
+
+//get paste button node
+let btnpaste = document.querySelector("#btnpaste")
+
+//get textarea input node
+let textareainput = document.querySelector("#textareainput")
+
+//get easy text output div node
+let easyoutput = document.querySelector("#easyoutput")
+
+//get main wrapper node
+//this is for adjusting width with buttons
+let mainwrapper = document.querySelector("#mainwrapper")
+
+//name of arabic font
+//this will help quickly use a new font from CSS
+//maybe use a font selector later
 let arabicfont = "Noto Sans Arabic"
 
+//lookup for maxWidth in localStorage
+//if not present assign 50rem to maxWidth
 if(localStorage.getItem('maxwidth')){
-  wrapper.style.maxWidth=localStorage.getItem('maxwidth')
+  mainwrapper.style.maxWidth=localStorage.getItem('maxwidth')
 }else{
-  wrapper.style.maxWidth='50rem'
+  mainwrapper.style.maxWidth='50rem'
 }
 
-incwidth.addEventListener('click',function(e){
-  wrapper.style.maxWidth = String(Number(wrapper.style.maxWidth.replace('rem', '')) + 0.1) + 'rem'
-  localStorage.setItem('maxwidth',wrapper.style.maxWidth)
+//listen for input on width adjustment buttons
+btnincwidth.addEventListener('click',function(e){
+  mainwrapper.style.maxWidth = String(Number(mainwrapper.style.maxWidth.replace('rem', '')) + 0.1) + 'rem'
+  localStorage.setItem('maxwidth',mainwrapper.style.maxWidth)
 })
 
-decwidth.addEventListener('click',function(e){
-  wrapper.style.maxWidth = String(Number(wrapper.style.maxWidth.replace('rem', '')) - 0.1) + 'rem'
-  localStorage.setItem('maxwidth',wrapper.style.maxWidth)
+btndecwidth.addEventListener('click',function(e){
+  mainwrapper.style.maxWidth = String(Number(mainwrapper.style.maxWidth.replace('rem', '')) - 0.1) + 'rem'
+  localStorage.setItem('maxwidth',mainwrapper.style.maxWidth)
 })
 
-arabiccheck.addEventListener('input', function(e) {
+//listen for arabic english checkbox
+chkboxaren.addEventListener('input', function(e) {
   if (e.target.checked) {
-    easy.style.fontFamily = arabicfont
-    easy.style.direction = "rtl"
+    easyoutput.style.fontFamily = arabicfont
+    easyoutput.style.direction = "rtl"
     localStorage.setItem('arabic', 'checked')
   } else {
-    easy.style.fontFamily = "easy"
-    easy.style.direction = "ltr"
+    easyoutput.style.fontFamily = "easy"
+    easyoutput.style.direction = "ltr"
     localStorage.setItem('arabic', 'unckecked')
   }
 })
 
+//lookup for arabic english checkbox value in localStorage
 if (localStorage.getItem('arabic')) {
   if (localStorage.getItem('arabic') === 'checked') {
-    easy.style.fontFamily = arabicfont
-    easy.style.direction = "rtl"
-    arabiccheck.checked = true
+    easyoutput.style.fontFamily = arabicfont
+    easyoutput.style.direction = "rtl"
+    chkboxaren.checked = true
   } else if (localStorage.getItem('arabic') === 'unchecked') {
-    easy.style.fontFamily = "easy"
-    easy.style.direction = "ltr"
-    arabiccheck.checked = false
+    easyoutput.style.fontFamily = "easy"
+    easyoutput.style.direction = "ltr"
+    chkboxaren.checked = false
   }
 }
 
-if (localStorage.getItem('maxwidth')) {
-  wrapper.style.maxWidth = localStorage.getItem('maxwidth')
+//lookup if there is text in localStorage for easy display
+//if there is, display it
+if (localStorage.getItem('easyoutput')) {
+  easyoutput.innerText = localStorage.getItem('easyoutput')
 }
 
-if (localStorage.getItem('easy')) {
-  easy.innerText = localStorage.getItem('easy')
-}
-
+//lookup localStorage for font color
+//if there is, set color of easy text and color picker for text color
+//if not, set it to black
 if (localStorage.getItem('color')) {
-  easy.style.color = localStorage.getItem('color')
-  fg.value = localStorage.getItem('color')
+  easyoutput.style.color = localStorage.getItem('color')
+  clrfg.value = localStorage.getItem('color')
 } else {
-  easy.style.color = "#000000"
+  easyoutput.style.color = "#000000"
 }
 
-if (localStorage.getItem('background')) {
-  easy.style.backgroundColor = localStorage.getItem('background')
-  bg.value = localStorage.getItem('background')
+//lookup localStorage for background color
+//if there is, set background color and color picker for background
+//if not, set it to white
+if (localStorage.getItem('backgroundcolor')) {
+  easyoutput.style.backgroundColor = localStorage.getItem('backgroundcolor')
+  clrbg.value = localStorage.getItem('backgroundcolor')
 } else {
-  easy.style.backgroundColor = "#ffffff"
+  easyoutput.style.backgroundColor = "#ffffff"
 }
 
-fg.addEventListener('input', function(e) {
-  easy.style.color = (e.target.value)
+//event listener for color picker for text color
+clrfg.addEventListener('input', function(e) {
+  easyoutput.style.color = (e.target.value)
   localStorage.setItem('color', e.target.value)
 })
 
-bg.addEventListener('input', function(e) {
-  easy.style.backgroundColor = (e.target.value)
-  localStorage.setItem('background', e.target.value)
+//event listener for color picker for background color
+clrbg.addEventListener('input', function(e) {
+  easyoutput.style.backgroundColor = (e.target.value)
+  localStorage.setItem('backgroundcolor', e.target.value)
 })
 
-txt.addEventListener("input", function() {
-  text = txt.value.replace(/-$\n/gm, "")
-  text = text.replace(/([^\.\?\!\:»])$\n/gm, "$1 ")
-  text = text.replace(/([\.\?\!\:»])$/gm, "$1\n")
-  easy.innerText = text
-  localStorage.setItem('easy', text)
+//function for processing input text
+//returns cleaned up text
+function cleanuptext(txt){
+  txt = txt.replace(/-$\n/gm, "")
+  txt = txt.replace(/([^\.\?\!\:»])$\n/gm, "$1 ")
+  txt = txt.replace(/([\.\?\!\:»])$/gm, "$1\n")
+  return txt
+}
+
+//get input from textarea, clean it up and display
+textareainput.addEventListener("input", function(e) {
+  let txt = cleanuptext(e.target.value)
+  easyoutput.innerText = txt
+  localStorage.setItem('easyoutput', txt)
 })
 
-paste.addEventListener("click", function() {
-  navigator.clipboard.readText().then(function(text) {
-    text = text.replace(/-$\n/gm, "")
-    text = text.replace(/([^\.\?\!\:»])$\n/gm, "$1 ")
-    text = text.replace(/([\.\?\!\:»])$/gm, "$1\n")
-    easy.innerText = text
-    localStorage.setItem('easy', text)
+//get input from clipboard when paste button is clicked
+btnpaste.addEventListener("click", function() {
+  navigator.clipboard.readText().then(function(txt) {
+    txt=cleanuptext(txt)
+    easyoutput.innerText = txt
+    localStorage.setItem('easyoutput', txt)
   })
 })
 
+//lookup localStorage for fontsize
+//if there is, use it
+//if not set font size to 1rem
 if (localStorage.getItem('fontsize')) {
-  easy.style.fontSize = localStorage.getItem('fontsize')
+  easyoutput.style.fontSize = localStorage.getItem('fontsize')
 } else {
-  easy.style.fontSize = "1rem"
+  easyoutput.style.fontSize = "1rem"
 }
 
-inc.addEventListener("click", function(e) {
-  easy.style.fontSize = String(Number(easy.style.fontSize.replace("rem", "")) + 0.1) + "rem"
-  localStorage.setItem('fontsize', easy.style.fontSize)
+//increase font size when button is clicked
+//update localstorage value of fontsize
+btnincfontsize.addEventListener("click", function(e) {
+  easyoutput.style.fontSize = String(Number(easyoutput.style.fontSize.replace("rem", "")) + 0.1) + "rem"
+  localStorage.setItem('fontsize', easyoutput.style.fontSize)
 })
 
-dec.addEventListener("click", function(e) {
-  easy.style.fontSize = String(Number(easy.style.fontSize.replace("rem", "")) - 0.1) + "rem"
-  localStorage.setItem('fontsize', easy.style.fontSize)
+//decrease font size when button is clicked
+//update localstorage value of fontsize
+btndecfontsize.addEventListener("click", function(e) {
+  easyoutput.style.fontSize = String(Number(easyoutput.style.fontSize.replace("rem", "")) - 0.1) + "rem"
+  localStorage.setItem('fontsize', easyoutput.style.fontSize)
 })
 
 document.body.addEventListener("keydown", function(ev) {
-
   // function to check the detection
   ev = ev || window.event; // Event object 'ev'
   let key = ev.which || ev.keyCode; // Detecting keyCode
-
   // Detecting Ctrl
   let ctrl = ev.ctrlKey ? ev.ctrlKey : ((key === 17) ? true : false);
-
   // If key pressed is V and if ctrl is true.
   if (key == 86 && ctrl) {
     // print in console.
-    navigator.clipboard.readText().then(function(text) {
-      text = text.replace(/-$\n/gm, "")
-      text = text.replace(/([^\.\?\!\:»])$\n/gm, "$1 ")
-      text = text.replace(/([\.\?\!\:»])$/gm, "$1\n")
-      easy.innerText = text
-      localStorage.setItem('easy', text)
+    navigator.clipboard.readText().then(function(txt) {
+      txt = cleanuptext(txt)
+      easyoutput.innerText = txt
+      localStorage.setItem('easyoutput', txt)
       window.scrollTo(0,0)
     })
   }
